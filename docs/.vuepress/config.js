9e47256f3c81c5100bb13656c62dc1d171552e0b
yx <y18583236050@163.com>
2021-05-13 16:45:33 +0800

    yx

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,8 @@
-// docs/.vuepress/config.js
-
 module.exports = {
-  // host: '0.0.0.0',  // 生成网页地址（本地调试使用）
-  // port: '22335',  // 生成网页端口（本地调试使用）
-  title: "Henryx's Blog", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-  description: "My World", // meta 中的描述文字，用于SEO
+//   host: "0.0.0.0", // 生成网页地址（本地调试使用）
+//   port: "22333", // 生成网页端口（本地调试使用）
+  title: "Tsanfer's Blog", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+  description: "现居住于猎户臂上的一个碳基生命", // meta 中的描述文字，用于SEO
   head: [
     ["link", { rel: "icon", href: "/favicon.svg" }], //浏览器的标签栏的网页图标,基地址/docs/.vuepress/public
     [
@@ -15,10 +13,10 @@ module.exports = {
       },
     ], //在移动端，搜索框在获得焦点时会放大
   ],
-
   theme: "reco", //选择主题‘reco’
   themeConfig: {
     type: "blog", //选择类型博客
+    fullscreen: true,
     blogConfig: {
       category: {
         location: 2, // 在导航栏菜单中所占的位置，默认2
@@ -28,129 +26,180 @@ module.exports = {
         location: 3, // 在导航栏菜单中所占的位置，默认3
         text: "标签", // 默认 “标签”
       },
+      socialLinks: [
+        { icon: "reco-github", link: "https://github.com/Tsanfer" },
+        { icon: "reco-bilibili", link: "https://space.bilibili.com/12167681" },
+        { icon: "reco-qq", link: "tencent://message/?uin=1124851454" },
+        { icon: "reco-twitter", link: "https://twitter.com/a1124851454" },
+        { icon: "reco-mail", link: "mailto:a1124851454@gmail.com" },
+      ],
     },
     nav: [
       //导航栏设置
       { text: "主页", link: "/", icon: "reco-home" },
       { text: "时间线", link: "/timeline/", icon: "reco-date" },
+      { text: "订阅", link: "https://tsanfer.xyz/rss.xml", icon: "reco-rss" },
+      {
+        text: "工具",
+        icon: "reco-api",
+        items: [
+          {
+            text: "个人网盘",
+            link: "http://clouddisk.tsanfer.xyz:8081",
+            icon: "fa-hdd",
+          },
+          {
+            text: "订阅转换器",
+            link: "https://tsanfer.xyz/sub-web/",
+            icon: "fa-exchange-alt",
+          },
+        ],
+      },
       {
         text: "联系",
         icon: "reco-message",
         items: [
           {
             text: "GitHub",
-            link: "https://github.com/yxHenry",
+            link: "https://github.com/Tsanfer",
             icon: "reco-github",
+          },
+          {
+            text: "CSDN",
+            link: "https://blog.csdn.net/qq_27961843/",
+            icon: "reco-csdn",
+          },
+          {
+            text: "BiliBili",
+            link: "https://space.bilibili.com/12167681",
+            icon: "reco-bilibili",
+          },
+          {
+            text: "QQ",
+            link: "tencent://message/?uin=1124851454",
+            icon: "reco-qq",
+          },
+          {
+            text: "Twitter",
+            link: "https://twitter.com/a1124851454",
+            icon: "reco-twitter",
+          },
+          {
+            text: "Gmail",
+            link: "mailto:a1124851454@gmail.com",
+            icon: "reco-mail",
           },
         ],
       },
     ],
-    sidebar: "auto", //在所有页面中启用自动生成侧栏
-    record: "蜀ICP备20015866号-1",
-    recordLink: "http://www.beian.miit.gov.cn/",
+    // sidebar: [
+    //   ['/views/FrontEnd/vuepress_theme_reco-Github_Actions.md','"reco'],
+    //   ['/views/FrontEndVuePress_GithubPages_TravisCI.md', 'TravisCI'],
+    //   ['/views/FrontEndSphinx_GitHub_ReadtheDocs.md', 'ReadtheDocs'],
+    // ],
+    sidebar: {
+      "/views/frontEnd/": [
+        {
+          title: "前端", // 必要的
+          // path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: [
+            "vuepress_theme_reco-Github_Actions",
+            "VuePress_GithubPages_TravisCI",
+            "Sphinx_GitHub_ReadtheDocs",
+          ],
+        },
+      ],
+      "/views/MCU/": [
+        {
+          title: "MCU", // 必要的
+          // path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: [
+            "Linux_board_NFS",
+            "First_Prepare_for_Lanqiao_Cup_MCU_Competition",
+          ],
+        },
+      ],
+      "/views/Computer/": [
+        {
+          title: "计算机", // 必要的
+          // path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: ["Storage_hardware"],
+        },
+      ],
+      "/views/Tool/": [
+        {
+          title: "工具", // 必要的
+          // path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          sidebarDepth: 2, // 可选的, 默认值是 1
+          children: ["ffmpeg", "scrcpy"],
+        },
+      ],
+    },
+    // displayAllHeaders: true, // 默认值：false
+    subSidebar: "auto",
+
+    record: "蜀ICP备20005033号-1",
+    recordLink: "https://icp.chinaz.com/home/info?host=tsanfer.xyz",
+    cyberSecurityRecord: "川公网安备 51110202000301号",
+    cyberSecurityLink:
+      "http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51110202000301",
     startYear: "2020", // 项目开始时间，只填写年份
     lastUpdated: "最后更新时间", // string | boolean
-    author: "Henryx",
-    authorAvatar: "/avatar.jpg", //作者头像
+    author: "Tsanfer",
+    authorAvatar: "/avatar.svg", //作者头像
     mode: "light", //默认显示白天模式
+    codeTheme: "okaidia", // default 'tomorrow'
+    smooth: "true", //平滑滚动
     // 评论设置
     valineConfig: {
       appId: process.env.LEANCLOUD_APP_ID,
       appKey: process.env.LEANCLOUD_APP_KEY,
     },
   },
-
   markdown: {
     lineNumbers: true, //代码显示行号
-  },
-  // 搜索设置
+  }, // 搜索设置
   search: true,
-  searchMaxSuggestions: 10,
-
-  // 插件
+  searchMaxSuggestions: 10, // 插件
   plugins: [
-    ["flowchart"], // 支持流程图
     [
-      "@vuepress-reco/vuepress-plugin-bgm-player", // BGM播放器
+      "meting",
       {
-        audios: [
-          // {
-          //   name: "他只是经过",
-          //   artist: "h3R3 / Felix Bennett",
-          //   url:
-          //     "http://m10.music.126.net/20200927112636/c0afc1c68bd06db6ee6fb807cb309b0e/ymusic/obj/w5zDlMODwrDDiGjCn8Ky/2653769041/9b24/36a6/ad6d/5389bfa776498530f34db6726c0ee467.mp3",
-          //   cover:
-          //     "http://p2.music.126.net/mX6DKxPxdt2nInfNCIz9Fw==/109951165015483353.jpg?param=130y130",
-          // },
-          // {
-          //   name: "谪仙",
-          //   artist: "伊格赛听 / 叶里",
-          //   url:
-          //     "http://m10.music.126.net/20200927113003/ab2393e50202d2ab7349400458b0ce39/ymusic/5158/0352/070f/f2f7908e014861dc0cee0e869561ea90.mp3",
-          //   cover:
-          //     "http://p2.music.126.net/X-ZQ6wkyaL9cJTiyxmDhuw==/109951164680974796.jpg?param=130y130",
-          // },
-          // {
-          //   name: "Faster Than Light",
-          //   artist: "Andreas Waldetoft / Mia Stegmar",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/Andreas%20Waldetoft%2CMia%20Stegmar%20-%20Faster%20Than%20Light.mp3",
-          //   cover:
-          //     "https://p1.music.126.net/Gxv6d9W4Yd9q9WNHPpi8rw==/1379887104073348.jpg",
-          // },
-          // {
-          //   name: "Dawn",
-          //   artist: "DDRKirby(ISQ)",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/Dawn%20-%20DDRKirby%28ISQ%29.mp3",
-          //   cover:
-          //     "https://p2.music.126.net/IPnqMCk8YaN9inwYV2bdgQ==/18774161044446693.jpg",
-          // },
-          // {
-          //   name: "TRON Legacy (End Titles)",
-          //   artist: "Daft Punk",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/Daft%20Punk%20-%20TRON%20Legacy%20%28End%20Titles%29.mp3",
-          //   cover:
-          //     "https://p2.music.126.net/qOOTIykbSLw9RHB0vI83GA==/737772302281958.jpg",
-          // },
-          // {
-          //   name: "Broken Boy",
-          //   artist: "Tonspender",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/Tonspender%20-%20Broken%20Boy.flac",
-          //   cover:
-          //     "https://p2.music.126.net/4TnTRyHqa3-D2H1UnOa00w==/109951163666994621.jpg",
-          // },
-          // {
-          //   name: "Life Of Sin Pt. 4",
-          //   artist: "MitiS",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/MitiS%20-%20Life%20Of%20Sin%20Pt.%204.mp3",
-          //   cover:
-          //     "https://p2.music.126.net/LmjTrSwvSLSNBsfFsQFO6g==/2533274793491743.jpg",
-          // },
-          // {
-          //   name: "Sea Of Voices (RAC Mix)",
-          //   artist: "Porter Robinson",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/Porter%20Robinson%20-%20Sea%20Of%20Voices%20%28RAC%20Mix%29.mp3",
-          //   cover:
-          //     "https://p1.music.126.net/zjQROkEUokU7iS5eUvnVZQ==/3264450027161111.jpg",
-          // },
-          // {
-          //   name: "New Lipstick",
-          //   artist: "The Kissaway Trail",
-          //   url:
-          //     "https://cdn-image.tsanfer.xyz/music/The%20Kissaway%20Trail%20-%20New%20Lipstick.flac",
-          //   cover:
-          //     "https://p2.music.126.net/VjN74c1hoYgPCEZ9DngeQw==/109951163772624643.jpg",
-          // },
-        ],
+        // metingApi: "https://meting.sigure.xyz/api/music",
+        meting: {
+          server: "netease",
+          type: "playlist",
+          mid: "4902520778",
+        },
+        aplayer: {
+          lrcType: 3,
+          theme: "#3489fd",
+        },
       },
     ],
-    ["vuepress-plugin-smooth-scroll"], // 平滑滚动
+    // ],
+    [
+      "social-share", //分享插件
+      {
+        networks: ["qq", "weibo", "twitter", "facebook", "email"], //分享类型
+        email: "a1124851454@gmail.com", //email地址
+        twitterUser: "a1124851454", //Twitter账号
+      },
+    ],
+    [
+      "@vuepress-reco/vuepress-plugin-rss", //RSS插件
+      {
+        site_url: "https://tsanfer.xyz", //网站地址
+        copyright: "Tsanfer", //版权署名
+      },
+    ],
+    ["flowchart"], // 支持流程图
+    // ["vuepress-plugin-smooth-scroll"], // 平滑滚动
     ["@vuepress/nprogress"], // 加载进度条
     ["reading-progress"], // 阅读进度条
+    ["vuepress-plugin-code-copy", true], //一键复制代码插件
   ],
 };
